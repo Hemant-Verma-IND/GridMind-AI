8842d5e926c00e472fb9de04950d1f6ce402f165
Hemant Verma <chandraverma7092@gmail.com>
2026-05-28 12:11:28 +0530

    Fix layout type declaration typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readof< {
+}: Readonly<{
   children: React.ReactNode;
-} >) {
+}>) {
   return (
     <html lang="en">
       <body className="antialiased">
